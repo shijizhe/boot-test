fe04bbcbc795a223b1787ccd84cc935f40d5446f
shijizhe <shijizhe@inspur.com>
2024-03-29 11:23:41 +0800

    用户登出，解除注释代码

--- src/main/java/com/ya/boottest/config/security/YaLogoutSuccessHandler.java
+++ src/main/java/com/ya/boottest/config/security/YaLogoutSuccessHandler.java
@@ -1,15 +1,22 @@
 package com.ya.boottest.config.security;
 
+import cn.hutool.core.util.StrUtil;
+import com.ya.boottest.utils.constants.auth.AuthConstants;
+import com.ya.boottest.utils.jwt.JwtUtils;
 import com.ya.boottest.utils.redis.RedisUtils;
+import com.ya.boottest.utils.result.BaseResult;
+import com.ya.boottest.utils.result.ResultEnum;
+import com.ya.boottest.utils.servlet.ServletUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import java.io.IOException;
+
+import static com.ya.boottest.utils.constants.auth.AuthConstants.REDIS_KEY_AUTH_TOKEN;
 
 /**
  * <p>
@@ -29,8 +36,7 @@ public class YaLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        throw new UsernameNotFoundException("xxxx");
-       /* final String authorization = request.getHeader(AuthConstants.TOKEN_HEADER);
+        final String authorization = request.getHeader(AuthConstants.TOKEN_HEADER);
         // 1.请求头中没有携带token
         if (StrUtil.isBlank(authorization)) {
             ServletUtils.renderResult(response, BaseResult.successWithMessage("没有登录信息，无需退出"));
@@ -51,6 +57,6 @@ public class YaLogoutSuccessHandler implements LogoutSuccessHandler {
         // 清空Redis
         redisUtils.delete(REDIS_KEY_AUTH_TOKEN + userId);
         log.info("YaLogoutSuccessHandler onLogoutSuccess");
-        ServletUtils.renderResult(response, BaseResult.successWithMessage("退出登录成功"));*/
+        ServletUtils.renderResult(response, BaseResult.successWithMessage("退出登录成功"));
     }
 }
